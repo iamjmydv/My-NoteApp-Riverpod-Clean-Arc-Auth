3387c7e5eb0dc7eb9e5c65c33558e6a0c94c43b1
iamjmtdv <127085144+iamjmydv@users.noreply.github.com>
2026-05-23 17:05:50 +0800

    added WidgetsFlutterBinding.ensureInitialized() + await Firebase.initializedApp()

--- my_noteapp_riverpod_clean_arc_auth/lib/main.dart
+++ my_noteapp_riverpod_clean_arc_auth/lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+
+  //Ensures the flutter engine and widget binding are fully initialized before calling any plugin code.
+  //Required whenever 'main' does have async work prior to  'runApp' (e.g Firebase setup)
+  WidgetsFlutterBinding.ensureInitialized();
+  // Boots the Firebase SDK using the platforms deafult configuration (google-services.json)
+  await Firebase.initializeApp();
+
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
