c8cdebb032ee3212e8e6483c632db48fad443ae0
jamXpress <127085144+iamjmydv@users.noreply.github.com>
2026-05-29 14:33:44 +0800

    added comment on core/provider / auth_provider.dart

--- my_noteapp_riverpod_clean_arc_auth/lib/core/providers/auth_providers.dart
+++ my_noteapp_riverpod_clean_arc_auth/lib/core/providers/auth_providers.dart
@@ -2,10 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+//-------------------- Firebase Auth -----------------------------------------------------------------------------------
 final firebaseAuthProvider = Provider<FirebaseAuth>(
   (ref) => FirebaseAuth.instance
 );
 
+//-------------------- Firebase Firestore -------------------------------------------------------------------------------
 final firestoreProvider = Provider<FirebaseFirestore>(
   (ref) => FirebaseFirestore.instance
 );
