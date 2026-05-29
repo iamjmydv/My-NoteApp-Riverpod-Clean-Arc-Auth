3e9bff8ac094ba1d96412e9aa95dca11b605cf72
jamXpress <127085144+iamjmydv@users.noreply.github.com>
2026-05-29 15:05:14 +0800

    added core/provider for auth

--- my_noteapp_riverpod_clean_arc_auth/lib/core/providers/auth_providers.dart
+++ my_noteapp_riverpod_clean_arc_auth/lib/core/providers/auth_providers.dart
@@ -1,14 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:my_noteapp_riverpod_clean_arc_auth/feature/auth/data/datasources/auth_remote_datasource.dart';
+import 'package:my_noteapp_riverpod_clean_arc_auth/feature/auth/data/repository/auth_repository_impl.dart';
+import 'package:my_noteapp_riverpod_clean_arc_auth/feature/auth/domain/usecases/login_user_usecase.dart';
+import 'package:my_noteapp_riverpod_clean_arc_auth/feature/auth/domain/usecases/sign_up_user_usecase.dart';
 
 //-------------------- Firebase Auth -----------------------------------------------------------------------------------
 final firebaseAuthProvider = Provider<FirebaseAuth>(
-  (ref) => FirebaseAuth.instance
+  (ref) => FirebaseAuth.instance,
 );
 
-//-------------------- Firebase Firestore -------------------------------------------------------------------------------
+//-------------------- Firebase Firestore ------------------------------------------------------------------------------
 final firestoreProvider = Provider<FirebaseFirestore>(
-  (ref) => FirebaseFirestore.instance
+  (ref) => FirebaseFirestore.instance,
+);
+
+//-------------------- Datasource --------------------------------------------------------------------------------------
+
+final authRemoteDatasourceProvider = Provider<AuthRemoteDatasource>(
+  (ref) => AuthRemoteDatasourceImpl(
+    auth: ref.watch(firebaseAuthProvider),
+    firestore: ref.watch(firestoreProvider),
+  ),
+);
+
+//-------------------- Repository --------------------------------------------------------------------------------------
+
+final authRepositoryProvider = Provider<AuthRepositoryImpl>(
+  (ref) => AuthRepositoryImpl(ref.watch(authRemoteDatasourceProvider)),
+);
+
+//-------------------- Sign Up -----------------------------------------------------------------------------------------
+final signUpUseCaseProvider = Provider<SignUpUserUseCase>(
+  (ref) => SignUpUserUseCase(ref.watch(authRepositoryProvider))
+);
+
+//-------------------- Login -------------------------------------------------------------------------------------------
+final loginUserUseCaseProvider = Provider<LoginUserUseCase>(
+  (ref) => LoginUserUseCase(ref.watch(authRepositoryProvider))
 );
