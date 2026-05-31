c93e122f759b78cc1f1730be98f3e8fbdc752b4e
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-05-31 16:56:12 +0800

    misc files

--- my_noteapp_riverpod_clean_arc_auth/lib/feature/auth/presentation/login/providers/login_controller.dart
+++ my_noteapp_riverpod_clean_arc_auth/lib/feature/auth/presentation/login/providers/login_controller.dart
@@ -1,3 +1,10 @@
-class LoginController {
+import 'dart:async';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_noteapp_riverpod_clean_arc_auth/feature/auth/presentation/login/providers/login_state.dart';
+
+class LoginController extends AsyncNotifier<LoginState> {
+  @override 
+  FutureOr<LoginState> build() => const LoginInitialState();
+
   
 }
